05591b40a2a5e3132564682c80a6ce4ba4383aa2
Luis Nabais <dextro@gmail.com>
2017-02-14 09:02:02 +0000

    Expose some settings in the status page

--- app/server/statusHandler.js
+++ app/server/statusHandler.js
@@ -1,3 +1,5 @@
+const SETTINGS = require('./config');
+
 const Playlist = require('./components/playlist');
 
 let HEALTHY = false;
@@ -11,6 +13,7 @@ function getStatus() {
         uptime: process.uptime(),
         details: {
             serverTime: +new Date(),
+            settings: getSettings(),
             playlist: {
                 current: pl.getCurrent(),
                 entries: pl.getEntries(),
@@ -22,6 +25,26 @@ function getStatus() {
 
 function setHealthy(status) {
     HEALTHY = !!status;
+}
+
+const SETTINGS_TO_EXPOSE = ['slack', 'youtube', 'playlist'];
+function getSettings() {
+    // Copy object
+    let copy = JSON.parse(JSON.stringify(SETTINGS));
+
+    // cast keys to boolean to avoid exposing them
+    copy.slack.token = !!copy.slack.token;
+    copy.youtube.key = !!copy.youtube.key;
+
+    let result = {};
+
+    for(let key in copy) {
+        if (copy.hasOwnProperty(key) && SETTINGS_TO_EXPOSE.indexOf(key) !== -1) {
+            result[key] = copy[key]
+        }
+    }
+
+    return result;
 }
 
 module.exports = {
